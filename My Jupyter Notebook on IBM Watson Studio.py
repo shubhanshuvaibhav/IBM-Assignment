191690c69e96b64a95a25b893fcf0f425e577286
Shubhanshu Vaibhav <102943583+shubhanshuvaibhav@users.noreply.github.com>
2022-08-07 23:29:32 +0530

    Add files via upload

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Shubhanshu Vaibhav**
+# 
+# I am Consultant
+
+# *I am interested in data science because want to gain more insights from data*
+
+# ###### Below code will give you the sum of two numbers
+
+# In[1]:
+
+
+print(2+2)
+
+
+# ![This is an image](https://i2.wp.com/techvidvan.com/tutorials/wp-content/uploads/sites/2/2020/04/top-python-libraries-a-data-scientist-need-to-know.jpg?fit=802%2C420&ssl=1)
+# 
+# - Pandas
+# - Numpy
+# - Matplotlib
+# - Seaborn
+# - Scikit-learn
+# 
+# [IBM watson studio wiki](https://en.wikipedia.org/wiki/IBM_Watson_Studio)
+
+# In[ ]:
+
+
+
+
